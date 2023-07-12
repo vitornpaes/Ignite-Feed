f3dcd43d645810a2c73d5cbea664a17fc95d0bd2
unknown <vitornoepaes@hotmail.com>
2023-07-12 11:44:10 -0300

    Vitor

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Header } from "./Header/Header.jsx";
+import { Header } from "./components/Header/Header";
+import './global.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
