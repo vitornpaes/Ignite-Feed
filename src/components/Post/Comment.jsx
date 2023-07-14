538ae0f191a4c5f6645bdcf7b2f50bf2c541a609
VitorPaes <vitornoepaes@hotmail.com>
2023-07-14 00:24:11 -0300

    14/07

--- src/components/Post/Comment.jsx
+++ src/components/Post/Comment.jsx
@@ -1,5 +1,5 @@
-import styles from './Comment.module.css';
-import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from "./Comment.module.css";
+import { ThumbsUp, Trash } from "phosphor-react";
 export function Comment() {
   return (
     <div className={styles.comment}>
@@ -9,11 +9,13 @@ export function Comment() {
           <div className={styles.header}>
             <div className={styles.authorAndTime}>
               <strong>Vitor Paes</strong>
-              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">Cerca de 1h atrás</time>
+              <time title="11 de Maio às 08:13h" dateTime="2022-05-11 08:13:00">
+                Cerca de 1h atrás
+              </time>
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </div>
 
@@ -27,5 +29,5 @@ export function Comment() {
         </footer>
       </div>
     </div>
-  )
+  );
 }
